e318ad1a736cc1aead017aa8b2ce02bda4810b27
Pigrabbit <donghyuk1003@gmail.com>
2020-04-02 19:41:21 +0900

    Add posts saving features and its test code
    API controller, Service and save request DTO are added.
    Make sure that use different DTO according to your purpose; Entity/Controller class

---
+++ src/main/java/com/pigrabbit/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.pigrabbit.springboot.service;
+
+import com.pigrabbit.springboot.domain.posts.PostsRepository;
+import com.pigrabbit.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
