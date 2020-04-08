5c68b230817bd7955d3ad43ebe66767bc1bfb9a9
Pigrabbit <donghyuk1003@gmail.com>
2020-04-08 21:05:56 +0900

    Add a view where user can see all posts

--- src/main/java/com/pigrabbit/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/pigrabbit/springboot/domain/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.pigrabbit.springboot.domain.posts;
+package com.pigrabbit.springboot.domain;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
--- src/main/java/com/pigrabbit/springboot/domain/posts/Posts.java
+++ src/main/java/com/pigrabbit/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.pigrabbit.springboot.domain.posts;
 
+import com.pigrabbit.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
